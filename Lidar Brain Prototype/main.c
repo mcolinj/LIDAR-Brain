07101abec38ed7afe4de35c7966cc45f040862f9
Charlie Monk <charliem2019@icloud.com>
2016-01-31 17:24:08 -0500

    Initial Commit

---
+++ Lidar Brain Prototype/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lidar Brain Prototype
+//
+//  Created by Christopher Stephen Monk on 1/31/16.
+//  Copyright © 2016 Christopher Stephen Monk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
