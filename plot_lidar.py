b1f72696fd7b836783207ce067ba0db1891f9f4a
Matthew Jones <mcolinj@mac.com>
2016-02-16 08:50:09 -0500

    Added plot_lidar script that plots files of lidar data.
    Plots the contents of all files passed on the command line
    one at a time.    Plots the data from 360 point polar data
    form.
    
    e.g.
    
    0,24.5
    1,36.9
    ...
    359,777.0
    
    Can be used to look at snapshots and for debugging.   The
    output file for this can be produced from a single
    collected lidar rotation data using the snippet:
    
    for p in rotation:
        p.put_data

---
+++ plot_lidar.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import pylab
+import pdb
+import sys
+
+def plot_file(fname):
+    """slurp lidar data from a txt file and plot it with pyplot"""
+    pdb.set_trace()
+    data = np.loadtxt(fname, delimiter=",")
+
+    # toss out the error data
+    error_free_data = np.array(filter(lambda x: x[1] < 777, data))
+
+    # extract the theta and radius and convert to radians
+    theta,radius = zip(*error_free_data)
+    theta = map(math.radians, theta)
+
+    # Now do the plot
+    fig = pylab.figure(figsize=(9,9))
+    ax = fig.add_subplot(111, projection='polar')
+    ax.set_rmax(240)
+    ax.grid(True)
+    ax.cla()
+    ax.plot(theta, radius, 'x', color='r', linewidth=3)
+    pylab.show()
+
+    for arg in sys.argv[1:]:
+        plot_file(arg)
+    
