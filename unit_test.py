c95df343fbefc62c65f263941f422b1899a05d01
Matthew Jones <mcolinj@mac.com>
2016-02-19 00:20:26 -0500

    Added tests for UDPCommunicationChannel.

--- unit_test.py
+++ unit_test.py
@@ -1,5 +1,6 @@
 from laser import *
 import binascii
+import pdb
 
 def test_reading():
     """unit test of the reading object to make sure it continues to work"""
@@ -21,4 +22,23 @@ def test_packet():
     assert p1.rpm == 256
     assert str(p1) == "5 256 10.00 20.00 40.00 777.00"
     assert p1.as_data() == "20,10.00\n21,20.00\n22,40.00\n23,777.00\n"
+
+
+def test_udp_communication_channel():
+    """Create a simple two-way communication channel and make sure it sends and receives"""
+    local  = UDPCommunicationChannel()
+    remote = UDPCommunicationChannel(local_port=local.remote_port, remote_port=local.local_port)
+
+    local_message = "Hello, Remote, I am Local."
+    local.send_to(local_message)
+    data, reply_address = remote.receive_from()
+    assert data == local_message
+    assert reply_address[0] == local.local_ip
+
+    # Now the remote can reply, and the sender can recognize it as a reply
+    remote.reply_to("ACK "+data, reply_address)
+
+    # original sender uses special receive_reply
+    data, reply_address = local.receive_reply()
+    assert data == "ACK "+local_message
                         
